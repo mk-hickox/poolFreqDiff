0d0aeb458d7c8543a7a570bd28670f53bf14b599
Axel <axel@axel-Precision-T1700>
2017-03-08 13:19:42 +0000

    update

--- poolFreqDiffTest_QBGLM.py
+++ poolFreqDiffTest_QBGLM.py
@@ -267,7 +267,7 @@ if __name__ == "__main__":
         lines = open(filnam, 'rb')
         scriptsdir=os.path.dirname(os.path.realpath(__file__))
         print 'suppressWarnings(library(methods))'
-        print 'source('+scriptsdir+'/poolFreqDiffTest.R")'
+        print 'source("'+scriptsdir+'/poolFreqDiffTest.R")'
         print '#Parameters: ',"npops =",npops,"nlevels =",nlevels,"mincnt =",mincnt,"min coverege =",minc,\
               "max coverage =",maxc,"rescale =",rescale,"scale =",scale,"zeroes =",zeroes
         for line in lines:
